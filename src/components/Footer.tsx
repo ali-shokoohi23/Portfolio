d99a553cb955d8d0e1020f2fcf7ac748cdffb158
ali-shokoohi23 <alibest330@gmail.com>
2024-09-03 11:28:06 +0330

    fix: unused imports removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
--- src/components/Home/Home2.tsx
+++ src/components/Home/Home2.tsx
@@ -3,7 +3,6 @@ import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
